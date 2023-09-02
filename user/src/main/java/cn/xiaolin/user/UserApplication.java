ad3c0a68e7f12e394243c075b2ff22cda1b47eef
xlxing <xlxing@bupt.edu.cn>
2023-09-02 13:44:16 +0800

    update .gitignore

---
+++ user/src/main/java/cn/xiaolin/user/UserApplication.java
@@ -0,0 +1,16 @@
+package cn.xiaolin.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xlxing
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
